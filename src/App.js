bd9eea6b313394d15956a2e42466df1255e6dd47
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-11-27 17:37:54 -0600

    added main component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Main from "./components/Main";
+import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import "./App.css";
@@ -7,10 +7,10 @@ import "./App.css";
 function App(){
 return (
 <div className="App">
-<Wrapper>
-<Header />
-{/* <Main /> */}
-</Wrapper>
+  <Wrapper>
+    <Header />
+    <Main />
+  </Wrapper>
 </div>
 );
 }
