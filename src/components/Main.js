b7a1a93ed82af02bd5434e61dfd62b3091dd72e2
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-11-27 18:03:28 -0600

    added dataarea component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-//import DataArea from "./DataArea";
+import DataArea from "./DataArea";
 //import "../styles/Main.css";
 
 export default class Main extends Component {
     render(){
         return(
             <>
-            {/* <DataArea /> */}
+            <DataArea />
             </>
         );
     }
