d1ef8a552f461bf276e8cca65cd8c245d0f09b50
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-11-27 18:10:57 -0600

    added search box

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,12 @@
 import React from "react";
-//import SearchBox from "./SearchBox.js";
+import SearchBox from "./SearchBox.js";
 //import "../styles/Nav.css";
 
 function Nav({ handleSearchChange }) {
     return (
         <nav className = "navbar navbar-expand navbar-light bg-light">
             <div className="navbar-collpase row" id="navbarNav">
-                {/* <SearchBox handleSearchChange={handleSearchChange}/> */}
+                <SearchBox handleSearchChange={handleSearchChange}/>
             </div>
         </nav>
     );
